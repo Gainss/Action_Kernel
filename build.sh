61f52cfb35e58544eb904e78e45b20ced1943b8b
Gainss <64727004+Gainss@users.noreply.github.com>
2022-02-24 18:20:41 +0800

    build.sh for Clang
    Use script building (Clang)

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 echo "===================Setup Export========================="
+export KERNEL_PATH=$PWD
+export CLANG_PATH=~/proton-clang
+export PATH=${CLANG_PATH}/bin:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
-export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/tool/arm/bin/arm-linux-androideabi-
+export SUBARCH=arm64
+
+echo "===================Setup Environment==================="
+git clone --depth=1 https://github.com/kdrag0n/proton-clang $CLANG_PATH
+sh -c "$(curl -sSL https://github.com/akhilnarang/scripts/raw/master/setup/android_build_env.sh/)"
 
 echo "=========================Clean========================="
 rm -rf $KERNEL_PATH/out/ *.zip
 make mrproper && git reset --hard HEAD
 
-echo "=========================make========================="
-make O=out lineageos_oneplus3_defconfig
-make O=out
+echo "=========================Make=========================="
+make O=out CC="ccache clang" CXX="ccache clang++" ARCH=arm64 CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld cepheus_defconfig
+make O=out CC="ccache clang" CXX="ccache clang++" ARCH=arm64 CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld 2>&1 | tee kernel.log
